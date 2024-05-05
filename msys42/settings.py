47ac7c4a5c6439c1301192f27f19f6b3234dc5df
Jinrooow <electrovfx@gmail.com>
2024-05-05 12:35:17 +0800

    migrated to mysql (settings.py for password)

--- msys42/settings.py
+++ msys42/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'msys42.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'NAME': 'CLEARdb',
+        'PASSWORD': '6091',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
