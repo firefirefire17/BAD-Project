1ccebf655d741502d1119f4f2b82135a9da06629
cisha <cishafernandez@gmail.com>
2024-05-05 19:11:30 +0800

    dashboard 80% complete

--- CLEAR/forms.py
+++ CLEAR/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from django.db import models
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import DateInput
 
 class RegisterForm(UserCreationForm):
     CHOICES = [('owner', 'Owner'), ('product_manager', 'Product Manager')]
