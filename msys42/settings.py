83c83249c8cc1e8fba542d6aafef34817d295565
Wenric <brycewenric08@gmail.com>
2024-05-05 15:55:58 +0800

    stable ver without mysql and account

--- msys42/settings.py
+++ msys42/settings.py
@@ -83,12 +83,8 @@ WSGI_APPLICATION = 'msys42.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': 'root',
-        'NAME': 'CLEARdb',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
